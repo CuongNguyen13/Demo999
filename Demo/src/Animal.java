64b0ada162b774b18fc8b497afe1b92192fa3c55
CuongNguyen13 <vancuong13032000@gmail.com>
2020-10-10 11:55:27 +0700

    chuc nang2

--- Demo/src/Animal.java
+++ Demo/src/Animal.java
@@ -2,9 +2,11 @@
 public class Animal {
 
 	public static void main(String[] args) {
-		
+		int count;
+		double chucnang2;
 		System.out.println("Chức năng 1 - Bảo làm");
 		System.out.println("chuc nang 2");
+		
 	}
 
 }
